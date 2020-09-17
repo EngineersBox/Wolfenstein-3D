20000843531fec75d50468d0aded14e5d95b6f38
u6940136@anu.edu.au <toastcraft.info@gmail.com>
2020-09-17 21:47:04 +1000

    Removed unused file reader implementations and fixed hashCode() method in texture.h

--- src/texturing/texture.h
+++ src/texturing/texture.h
@@ -34,81 +34,6 @@ class Texture {
         int height;
 };
 
-int imageLoad(const char *filename, Image *image) {
-    FILE *file;
-    unsigned long size;         // size of the image in bytes.
-    unsigned long i;            // standard counter.
-    unsigned short int planes;  // number of planes in image (must be 1)
-    unsigned short int bpp;     // number of bits per pixel (must be 24)
-    char temp;                  // temporary color storage for bgr-rgb conversion.
-
-    // make sure the file is there.
-    if ((file = fopen(filename, "rb")) == NULL) {
-        throw LoadImageError(filename, ImageCause::FILEEXISTS, (unsigned short)NULL);
-        return 0;
-    }
-
-    // seek through the bmp header, up to the width/height:
-    fseek(file, 18, SEEK_CUR);
-
-    // read the width
-    if ((i = fread(&image->width, 4, 1, file)) != 1) {
-        throw LoadImageError(filename, ImageCause::WIDTH, image->width);
-        return 0;
-    }
-
-    // read the height
-    if ((i = fread(&image->height, 4, 1, file)) != 1) {
-        throw LoadImageError(filename, ImageCause::HEIGHT, image->height);
-        return 0;
-    }
-
-    // calculate the size (assuming 24 bits or 3 bytes per pixel).
-    size = image->width * image->height * 3;
-
-    // read the planes
-    if ((fread(&planes, 2, 1, file)) != 1) {
-        throw LoadImageError(filename, ImageCause::PLANE, planes);
-        return 0;
-    }
-    if (planes != 1) {
-        throw LoadImageError("is not ", filename, ImageCause::PLANE, 1);
-        return 0;
-    }
-
-    // read the bitsperpixel
-    if ((i = fread(&bpp, 2, 1, file)) != 1) {
-        throw LoadImageError(filename, ImageCause::BPP, bpp);
-        return 0;
-    }
-    if (bpp != 24) {
-        throw LoadImageError("is not ", filename, ImageCause::BPP, 24);
-        return 0;
-    }
-
-    // seek past the rest of the bitmap header.
-    fseek(file, 24, SEEK_CUR);
-    // read the data.
-    image->data = (char *) malloc(size);
-
-    if (image->data == NULL) {
-        throw MemoryError("Error allocating memory for color-corrected image data");
-        return 0;
-    }
-    if ((i = fread(image->data, size, 1, file)) != 1) {
-        throw LoadImageError(filename, ImageCause::IDATA, (unsigned short)0);
-        return 0;
-    }
-    for (i = 0; i < size; i += 3) {  // reverse all of the colors. (bgr -> rgb)
-        temp = image->data[i];
-        image->data[i] = image->data[i + 2];
-        image->data[i + 2] = temp;
-    }
-
-    // we're done.
-    return 1;
-}
-
 Texture::Texture() {}
 
 Texture::Texture(string filename, string name, int width, int height) {
@@ -121,32 +46,14 @@ Texture::Texture(string filename, string name, int width, int height) {
 
 Texture::~Texture() {}
 
-Image* Texture::loadTexture() {
-    Image *image = (Image *) malloc(sizeof(Image));
-
-    if (image == NULL) {
-        throw MemoryError("Error allocating space for image");
-        exit(0);
-    }
-
-    if (!imageLoad((this->filename).c_str(), image)) {
-        exit(1);
-    }
-
-    return image;
-}
-
 long long Texture::hashCode() {
     // Hashing using rolling polynomial method
     string texString = name;
     texString.append(to_string(width));
     texString.append(to_string(height));
-    // for (int x = 0; x < texture.GetNumRows(); x++) {
-    //     for (int y = 0; y < texture.GetNumCols(); y++) {
-    //         texString.append("t");
-    //         // texString.append(to_string((int)texture.GetRgbPixel(x, y)));
-    //     }
-    // }
+    for (uint8_t elem : texture.data) {
+        texString.append(to_string(elem));
+    }
 
     long long hashVal = 0;
     for (int i = 0; i < texString.length(); i++) {
